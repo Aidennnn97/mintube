f73fde1f44ca1c723a808f84d12d93e6d1130f9d
Kim Min Sung <aidennnn97@gmail.com>
2022-09-20 20:27:43 +0900

    Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -84,5 +84,6 @@ export const postUpload = async(req, res) => {
 
 export const deleteVideo = async(req, res) => {
     const {id} = req.params;
+    await Video.findByIdAndDelete(id);
     return res.redirect("/");
 };
